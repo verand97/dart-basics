299a2890749d661bb105dcede18df7e59cb5c100
Verrr30 <veramaofficial@gmail.com>
2026-03-08 11:41:48 +0700

    string

---
+++ dart_project/lib/string.dart
@@ -0,0 +1,10 @@
+void main() {
+  String depan = 'Verri';
+  String belakang = 'Andika';
+  
+  // Menggabungkan string (Concatenation)
+  String namaLengkap = '$depan $belakang';
+  
+  print('Halo, nama saya $namaLengkap');
+  print('Versi Huruf Besar: ${namaLengkap.toUpperCase()}');
+}
