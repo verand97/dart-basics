e5a7d2290605ddab0330c8e31ce257b5fa48e1b1
Verrr30 <veramaofficial@gmail.com>
2026-03-08 12:14:36 +0700

    boolean

---
+++ dart_project/lib/boolean.dart
@@ -0,0 +1,9 @@
+void main() {
+
+  bool isLampuNyala = true; 
+  
+  bool isLampuMati = !isLampuNyala; 
+
+  print('Apakah lampu nyala? $isLampuNyala');
+  print('Apakah lampu mati? $isLampuMati');
+}
