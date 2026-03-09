c94fbe2895e19a457c02fc4b5c0b8ef077c2941a
Verrr30 <veramaofficial@gmail.com>
2026-03-09 13:14:22 +0700

    List

---
+++ dart_project/lib/list.dart
@@ -0,0 +1,10 @@
+void main() {
+  List keranjang = ['Apel', 'Jeruk', 'Mangga'];
+
+  print(keranjang[0]); 
+  
+  keranjang.add('Pisang');
+  
+  print('Total buah: ${keranjang.length}');
+  print(keranjang);
+}
